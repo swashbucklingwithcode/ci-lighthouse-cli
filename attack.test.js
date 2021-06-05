44c6c977219745d2745ce1af8f183c95100e9a59
Jimmy DC <jimmydalecleveland@gmail.com>
2021-06-05 14:32:55 -0600

    add jest and ci for it

---
+++ attack.test.js
@@ -0,0 +1,7 @@
+const attack = require('./attack')
+
+test('should return damage sentence when called correctly', () => {
+  const result = attack('Smelly Ogre', 23)
+  expect(result).toMatch(/^Smelly Ogre(.*)23/)
+  expect(result).toMatchInlineSnapshot(`"Smelly Ogre dealt 23 damage!"`)
+})
