618106c6d0687c212df06ecf54244c2686980b0f
Jimmy DC <jimmydalecleveland@gmail.com>
2021-06-05 14:36:17 -0600

    ran linting on new test

--- attack.test.js
+++ attack.test.js
@@ -1,7 +1,7 @@
-const attack = require('./attack')
+const attack = require('./attack');
 
 test('should return damage sentence when called correctly', () => {
-  const result = attack('Smelly Ogre', 23)
-  expect(result).toMatch(/^Smelly Ogre(.*)23/)
-  expect(result).toMatchInlineSnapshot(`"Smelly Ogre dealt 23 damage!"`)
-})
+  const result = attack('Smelly Ogre', 23);
+  expect(result).toMatch(/^Smelly Ogre(.*)23/);
+  expect(result).toMatchInlineSnapshot('"Smelly Ogre dealt 23 damage!"');
+});
