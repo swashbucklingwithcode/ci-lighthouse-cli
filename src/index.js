bec5d72a3f3166b2de8121154a49ef4cae977175
Jimmy DC <jimmydalecleveland@gmail.com>
2021-06-17 14:26:05 -0600

    add html website functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import attack from './attack';
 
-console.log(
-  attack('Smelly Ogre', 23),
-);
+const resultTag = document.querySelector('#resultTag');
+const creature = document.querySelector('#creature');
+const damage = document.querySelector('#damage');
+const resultButton = document.querySelector('#resultButton');
+
+resultButton.addEventListener('click', () => {
+  const result = attack(creature.value, damage.value);
+  resultTag.innerHTML = result;
+});
