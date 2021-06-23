fb06a0006f4fc25da8a382942f7cacf7bb8befef
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2021-06-23 11:36:08 -0600

    extra lighthouse ci restrictions added

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,7 +6,11 @@ module.exports = {
     assert: {
       preset: 'lighthouse:no-pwa',
       assertions: {
-        'csp-xss': 'off',
+        'csp-xss': 'warn',
+        'categories:performance': ['error', { minScore: 0.95 }],
+        'categories:accessibility': ['error', { minScore: 0.95 }],
+        'categories:best-practices': ['error', { minScore: 0.95 }],
+        'categories:seo': ['error', { minScore: 1 }],
       },
     },
   },
